8fc746ad29ad1a0eaf562507b23a2f0cc91fca57
HaseebAhmed7949 <66886913+HaseebAhmed49@users.noreply.github.com>
2023-04-10 09:06:52 +0500

    Created React TypeScript FrontEnd Project
    Adding Basic Routing and Navbar as well

---
+++ eCart.UI/ecart.ui/src/app/Components/About.tsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import logo from './logo.svg';
+
+function About() {
+  return (
+    <div className="container">
+        About
+    </div>
+  );
+}
+
+export default About;
---
+++ eCart.UI/ecart.ui/src/app/Components/Login.tsx
@@ -0,0 +1,10 @@
+
+function Login() {
+  return (
+    <div className="container">
+        Login
+    </div>
+  );
+}
+
+export default Login;
