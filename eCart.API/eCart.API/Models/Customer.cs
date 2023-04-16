e8119c08574a3bd8432c81be3d01fbfd72cdd9a3
HaseebAhmed7949 <66886913+HaseebAhmed49@users.noreply.github.com>
2023-04-16 23:55:02 +0500

    Added Customer Model

---
+++ eCart.API/eCart.API/Models/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace eCart.API.Models
+{
+	public class Customer
+	{
+		public string Id { get; set; }
+
+		public string Name { get; set; }
+
+		public string Address { get; set; }
+
+		public string ContactNo { get; set; }
+
+		public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+		public DateTime UpdatedAt { get; set; }
+	}
+}
+
