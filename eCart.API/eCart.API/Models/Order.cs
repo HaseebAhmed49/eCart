d3bc5caea26cc619a160aa9fc28623fa7796f580
HaseebAhmed7949 <66886913+HaseebAhmed49@users.noreply.github.com>
2023-04-17 23:44:01 +0500

    Added Order Entity

---
+++ eCart.API/eCart.API/Models/Order.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace eCart.API.Models
+{
+	public class Order
+	{
+		public int Id { get; set; }
+
+		public int CustomerId { get; set; }
+
+		public OrderStatus OrderStatus { get; set; }
+	}
+
+	public enum OrderStatus
+	{
+		Delievered,
+		Closed,
+		Hold,
+		Shipped,
+		New
+	}
+}
+
