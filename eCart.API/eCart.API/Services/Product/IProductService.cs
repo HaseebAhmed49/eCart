c1b5c1e5be210b4fa6a221ee590e35aed1783db6
HaseebAhmed7949 <66886913+HaseebAhmed49@users.noreply.github.com>
2023-04-21 23:24:37 +0500

    Working on ProductServices

---
+++ eCart.API/eCart.API/Services/Product/IProductService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using eCart.API.Models.Product;
+
+namespace eCart.API.Services.Product
+{
+	public interface IProductService
+	{
+		public Task AddProduct(Product product);
+	}
+}
+
