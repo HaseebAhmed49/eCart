e10df3a292113592880ccc059c72ec7e35a05762
HaseebAhmed7949 <66886913+HaseebAhmed49@users.noreply.github.com>
2023-04-27 23:58:19 +0500

    Working on Product Services

---
+++ eCart.API/eCart.API/Services/ProductService/ProductService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using eCart.API.Models;
+
+namespace eCart.API.Services.ProductService
+{
+	public class ProductService: IProductService
+	{
+		public ProductService()
+		{
+		}
+
+        public Task AddProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteProductById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task GetProductById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task GetProducts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateProductById(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
