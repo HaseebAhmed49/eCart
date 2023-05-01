9cd1b6c9eefd4e343bdefbd010115064b9c8c491
HaseebAhmed7949 <66886913+HaseebAhmed49@users.noreply.github.com>
2023-05-02 00:17:52 +0500

    Initial Migrations

--- eCart.API/eCart.API/Data/StoreContext.cs
+++ eCart.API/eCart.API/Data/StoreContext.cs
@@ -1,11 +1,16 @@
-﻿using System;
+﻿using eCart.API.Models;
+using Microsoft.EntityFrameworkCore;
+
 namespace eCart.API.Data
 {
-	public class StoreContext
+	public class StoreContext: DbContext
 	{
-		public StoreContext()
+		public StoreContext(DbContextOptions options): base(options)
 		{
+
 		}
+
+		public DbSet<Product> Products { get; set; }
 	}
 }
 
