e80f5174cc2f56f6bb5505f034f315f848322da5
HaseebAhmed7949 <66886913+HaseebAhmed49@users.noreply.github.com>
2023-05-03 22:38:39 +0500

    Working on Products Controller

--- eCart.API/eCart.API/Controllers/ProductsController.cs
+++ eCart.API/eCart.API/Controllers/ProductsController.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using eCart.API.Data;
+using eCart.API.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -12,16 +15,26 @@ namespace eCart.API.Controllers
     [Route("api/[controller]")]
     public class ProductsController : ControllerBase
     {
-        [HttpGet]
-        public string GetProducts()
+
+        private readonly StoreContext _context;
+
+        public ProductsController(StoreContext context)
         {
-            return "this will be a list of products";
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            var products = await _context.Products.ToListAsync();
+
+            return products;
         }
 
         [HttpGet("{id}")]
-        public string GetProducts(int id)
+        public async Task<ActionResult<Product>> GetProducts(int id)
         {
-            return "this will be a product";
+            return await _context.Products.FindAsync(id);
         }
 
     }
