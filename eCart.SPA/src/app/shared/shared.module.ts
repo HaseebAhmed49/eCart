53cfa4f7ee82f9cdd9487dd92f53b3f60ccc1bee
HaseebAhmed7949 <66886913+HaseebAhmed49@users.noreply.github.com>
2023-05-26 02:22:41 +0500

    Added Pagination in Client

--- eCart.SPA/src/app/shared/shared.module.ts
+++ eCart.SPA/src/app/shared/shared.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PaginationModule} from 'ngx-bootstrap/pagination';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    // forRoot is used to be as a singleton. Shared by other components as well.
+    PaginationModule.forRoot()
+  ],
+  exports: [
+    PaginationModule
   ]
 })
 export class SharedModule { }
--- eCart.SPA/src/app/shop/shop.service.ts
+++ eCart.SPA/src/app/shop/shop.service.ts
@@ -3,6 +3,7 @@ import { Injectable, Type } from '@angular/core';
 import { Brand } from '../shared/models/brand';
 import { Pagination } from '../shared/models/pagination';
 import { Product } from '../shared/models/products';
+import { ShopParams } from '../shared/models/shopParams';
 import { Types } from '../shared/models/type';
 
 @Injectable({
@@ -14,12 +15,14 @@ export class ShopService {
 
   constructor(private http: HttpClient) { }
 
-    getProducts(brandId?: number, typeId?: number, sort?:string){
+    getProducts(shopParams: ShopParams){
       let params = new HttpParams();
   
-      if(brandId) params = params.append('brandId',brandId);
-      if(typeId) params = params.append('typeId',typeId);
-      if(sort) params = params.append('sort',sort);
+      if(shopParams.brandId > 0) params = params.append('brandId',shopParams.brandId);
+      if(shopParams.typeId > 0) params = params.append('typeId',shopParams.typeId);
+      params = params.append('sort',shopParams.sort);
+      params = params.append('pageIndex',shopParams.pageNumber);
+      params = params.append('pageSize',shopParams.pageSize);
   
     return this.http.get<Pagination<Product[]>>(this.baseUrl + 'products', {params});
   }
