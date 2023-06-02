fa35491708da69a0f57d6314c10c423297d3887c
HaseebAhmed7949 <66886913+HaseebAhmed49@users.noreply.github.com>
2023-06-03 03:10:54 +0500

    Adding Loading Indicators
    ng add ngx-spinner

--- eCart.SPA/src/app/core/core.module.ts
+++ eCart.SPA/src/app/core/core.module.ts
@@ -7,7 +7,8 @@ import { NotFoundComponent } from './not-found/not-found.component';
 import { ServerErrorComponent } from './server-error/server-error.component';
 import { ToastrModule } from 'ngx-toastr';
 import { SectionHeaderComponent } from './section-header/section-header.component';
-import { BreadcrumbComponent, BreadcrumbModule } from 'xng-breadcrumb';
+import { BreadcrumbModule } from 'xng-breadcrumb';
+import { NgxSpinnerModule } from 'ngx-spinner';
 
 
 
@@ -26,11 +27,13 @@ import { BreadcrumbComponent, BreadcrumbModule } from 'xng-breadcrumb';
       positionClass: 'toast-bottom-right',
       preventDuplicates: true
     }),
-    BreadcrumbModule
+    BreadcrumbModule,
+    NgxSpinnerModule
   ],
   exports:[
     NavBarComponent,
-    SectionHeaderComponent
+    SectionHeaderComponent,
+    NgxSpinnerModule
   ]
 })
 export class CoreModule { }
