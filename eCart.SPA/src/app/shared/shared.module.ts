66e05825f127d94664772868d6bb403637baf5e9
HaseebAhmed7949 <66886913+HaseebAhmed49@users.noreply.github.com>
2023-07-06 00:43:54 +0500

    199: Building Reusable Text Input component

--- eCart.SPA/src/app/shared/shared.module.ts
+++ eCart.SPA/src/app/shared/shared.module.ts
@@ -6,14 +6,16 @@ import { PagerComponent } from './pager/pager.component';
 import { CarouselModule } from 'ngx-bootstrap/carousel';
 import { OrderTotalsComponent } from './order-totals/order-totals.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown'
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { TextInputComponent } from './components/text-input/text-input.component'
 
 
 @NgModule({
   declarations: [
     PagingHeaderComponent,
     PagerComponent,
-    OrderTotalsComponent
+    OrderTotalsComponent,
+    TextInputComponent
   ],
   imports: [
     CommonModule,
@@ -30,7 +32,8 @@ import { BsDropdownModule } from 'ngx-bootstrap/dropdown'
     CarouselModule,
     OrderTotalsComponent,
     ReactiveFormsModule,
-    BsDropdownModule
+    BsDropdownModule,
+    TextInputComponent
   ]
 })
 export class SharedModule { }
