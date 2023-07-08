c6f3c3726b00eb77fd327d4fd5578b87d75eca08
HaseebAhmed7949 <66886913+HaseebAhmed49@users.noreply.github.com>
2023-07-08 21:59:45 +0500

    210: Configured the order Entities for EF

---
+++ eCart.API/eCart.API/Data/Config/DeliveryMethodConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using eCart.API.Data.Models.OrderAggregate;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace eCart.API.Data.Config
+{
+    public class DeliveryMethodConfiguration : IEntityTypeConfiguration<DeliveryMethod>
+    {
+        public void Configure(EntityTypeBuilder<DeliveryMethod> builder)
+        {
+            builder.Property(d => d.Price)
+                .HasColumnType("decimal(18,2)");
+        }
+    }
+}
+
---
+++ eCart.API/eCart.API/Data/Config/OrderItemConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using eCart.API.Data.Models.OrderAggregate;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace eCart.API.Data.Config
+{
+	public class OrderItemConfiguration: IEntityTypeConfiguration<OrderItem>
+	{
+
+        public void Configure(EntityTypeBuilder<OrderItem> builder)
+        {
+            builder.OwnsOne(i => i.ItemOrdered, io =>
+            {
+                io.WithOwner();
+            });
+
+            builder.Property(i => i.Price)
+                .HasColumnType("decimal(18.2)");
+
+
+        }
+    }
+}
+
