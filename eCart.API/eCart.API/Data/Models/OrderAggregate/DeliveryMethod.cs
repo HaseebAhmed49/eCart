723772961422d3f5aa401e121b5ef77b91ca61b8
HaseebAhmed7949 <66886913+HaseebAhmed49@users.noreply.github.com>
2023-07-08 21:48:47 +0500

    209: Order Aggregate
    Order Related Entities are created

---
+++ eCart.API/eCart.API/Data/Models/OrderAggregate/DeliveryMethod.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using eCart.API.Models;
+
+namespace eCart.API.Data.Models.OrderAggregate
+{
+	public class DeliveryMethod: BaseEntity
+	{
+		public string ShortName { get; set; }
+
+		public string DeliveryTime { get; set; }
+
+		public string Description { get; set; }
+
+		public decimal Price { get; set; }
+	}
+}
---
+++ eCart.API/eCart.API/Data/Models/OrderAggregate/OrderStatus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace eCart.API.Data.Models.OrderAggregate
+{
+	public enum OrderStatus
+	{
+		[EnumMember(Value = "Pending")]
+		Pending,
+
+        [EnumMember(Value = "Payment Received")]
+        PaymentReceived,
+
+        [EnumMember(Value = "Payment Failed")]
+        PaymentFailed
+    }
+}
