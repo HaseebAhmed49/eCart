6bb9e3f89bba112988cdd6cf749642091113987f
HaseebAhmed7949 <66886913+HaseebAhmed49@users.noreply.github.com>
2023-07-19 00:33:06 +0500

    257: Payment Controller

---
+++ eCart.API/eCart.API/Controllers/PaymentsController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using eCart.API.Data.Models;
+using eCart.API.Data.Services.PaymentService;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace eCart.API.Controllers
+{
+    public class PaymentsController : BaseApiController
+    {
+        private readonly IPaymentService _paymentService;
+
+        public PaymentsController(IPaymentService paymentService)
+        {
+            _paymentService = paymentService;
+        }
+
+        [Authorize]
+        [HttpPost("{basketId}")]
+        public async Task<ActionResult<CustomerBasket>> CreateOrUpdatePaymentIntent(string basketId)
+        {
+            return await _paymentService.CreateOrUpdatePaymentIntent(basketId);
+        }
+
+    }
+}
+
--- eCart.API/eCart.API/Data/DTOs/Basket/CustomerBasketDTO.cs
+++ eCart.API/eCart.API/Data/DTOs/Basket/CustomerBasketDTO.cs
@@ -10,6 +10,12 @@ namespace eCart.API.Data.DTOs.Basket
         public string Id { get; set; }
         
         public List<BasketItemDTO> Items { get; set; }
+
+        public int? DeliveryMethodId { get; set; }
+
+        public string ClientSecret { get; set; }
+
+        public string PaymentIntentId { get; set; }
     }
 }
 
