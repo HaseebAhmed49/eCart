c2ad9d73acc57efc466d90cc446869170ca1bbaa
HaseebAhmed7949 <66886913+HaseebAhmed49@users.noreply.github.com>
2023-07-21 00:23:53 +0500

    270: Loading indicators / Enabling Linear Mode

--- eCart.SPA/src/app/core/interceptors/loading.interceptor.ts
+++ eCart.SPA/src/app/core/interceptors/loading.interceptor.ts
@@ -14,9 +14,13 @@ export class LoadingInterceptor implements HttpInterceptor {
   constructor(private busyService: BusyService) {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    if(!request.url.includes('email')){
-      this.busyService.busy();
+    if(
+      request.url.includes('email') || 
+      request.method == 'POST' && request.url.includes('orders')
+      ){
+        return next.handle(request);
     }
+    this.busyService.busy();
     return next.handle(request).pipe(
       delay(1000),
       finalize( () => this.busyService.idle())
