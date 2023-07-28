0f912d3ccdf7a4a807003e0830140347b5224905
HaseebAhmed7949 <66886913+HaseebAhmed49@users.noreply.github.com>
2023-07-28 21:46:03 +0500

    Minor changes in Checkout-success component

--- eCart.SPA/src/app/checkout/checkout-success/checkout-success.component.ts
+++ eCart.SPA/src/app/checkout/checkout-success/checkout-success.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { Order } from 'src/app/shared/models/order';
 
 @Component({
   selector: 'app-checkout-success',
@@ -7,10 +8,13 @@ import { Router } from '@angular/router';
   styleUrls: ['./checkout-success.component.scss']
 })
 export class CheckoutSuccessComponent {
+  order?: Order;
 
   constructor(private router: Router){}
 
   ViewOrder(){
-    this.router.navigate(['orders']);
+    const navigation = this.router.getCurrentNavigation();
+    this.order = navigation?.extras?.state as Order;
+    // this.router.navigate(['orders']);
   }
 }
