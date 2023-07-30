746300c26e9e48d8760afac86861c33d9f9a81af
HaseebAhmed7949 <66886913+HaseebAhmed49@users.noreply.github.com>
2023-07-31 01:08:13 +0500

    Declared Method Name in Mail Interface

---
+++ eCart.API/eCart.API/Data/Services/Mail/IMailService.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace eCart.API.Data.Services.Mail
+{
+	public interface IMailService
+	{
+            Task SendEmailAsync(string toEmail, string subject, string content, string Name);
+    }
+}
+
