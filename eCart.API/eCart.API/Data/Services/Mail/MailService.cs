48baaae79cc584c8191abd2eb33bbaf7624d6906
HaseebAhmed7949 <66886913+HaseebAhmed49@users.noreply.github.com>
2023-08-01 01:10:32 +0500

    Mail Interface Method Implementation

---
+++ eCart.API/eCart.API/Data/Services/Mail/MailService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net.Mail;
+
+namespace eCart.API.Data.Services.Mail
+{
+	public class MailService: IMailService
+	{
+
+        public async Task SendEmailAsync(string toEmail, string subject, string content, string Name)
+        {
+            var apiKey = "XYZ";
+            var client = new SendGridClient(apiKey);
+            var from = new EmailAddress("haseebahmed02@gmail.com", Name);
+            var to = new EmailAddress(toEmail);
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, content, content);
+            var response = await client.SendEmailAsync(msg);
+        }
+	}
+}
