3e290b46480938bb041b8e30f9e9d872ecad028e
HaseebAhmed7949 <66886913+HaseebAhmed49@users.noreply.github.com>
2023-08-07 23:32:05 +0500

    Brevo (SendInBlue) support addition for emails

--- eCart.API/eCart.API/Data/Services/Mail/IMailService.cs
+++ eCart.API/eCart.API/Data/Services/Mail/IMailService.cs
@@ -3,7 +3,9 @@ namespace eCart.API.Data.Services.Mail
 {
 	public interface IMailService
 	{
-            Task SendEmailAsync(string toEmail, string subject, string content, string Name);
+        Task SendEmailAsync(string toEmail, string subject, string content, string Name);
+
+        Task SendEmailAsyncBrevo(string toEmail, string subject, string content, string Name);
     }
 }
 
