c98abb64d35c7e4e9a072eb23d5be3921d5a8aab
HaseebAhmed7949 <66886913+HaseebAhmed49@users.noreply.github.com>
2023-08-08 23:33:39 +0500

    Working on Email Send Method for Brevo

--- eCart.API/eCart.API/Data/Services/Mail/MailService.cs
+++ eCart.API/eCart.API/Data/Services/Mail/MailService.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Net.Mail;
 
 namespace eCart.API.Data.Services.Mail
@@ -15,5 +16,37 @@ namespace eCart.API.Data.Services.Mail
             var msg = MailHelper.CreateSingleEmail(from, to, subject, content, content);
             var response = await client.SendEmailAsync(msg);
         }
-	}
+
+        public Task SendEmailAsyncBrevo(string toEmail, string subject, string content, string Name)
+        {
+            Configuration.Default.ApiKey.Add("api-key", "YOUR API KEY");
+
+            var apiInstance = new TransactionalEmailsApi();
+            string tag = null;
+            string senderName = "John Doe";
+            string senderEmail = "example@example.com";
+            string templateName = "Example Template";
+            string htmlContent = "<html><body><h1>This is my first transactional email</h1></body></html>";
+            string htmlUrl = null;
+            string subject = "New Subject";
+            string replyTo = "replyto@domain.com";
+            string toField = "example@example.com";
+            bool? isActive = true;
+            string attachmentUrl = "https://example.net/upload-file";
+            CreateSmtpTemplateSender sender = new CreateSmtpTemplateSender(senderName, senderEmail);
+            try
+            {
+                var smtpTemplate = new CreateSmtpTemplate(tag, sender, templateName, htmlContent, htmlUrl, subject, replyTo, toField, attachmentUrl, isActive);
+                CreateModel result = apiInstance.CreateSmtpTemplate(smtpTemplate); Debug.WriteLine(result.ToJson());
+                Console.WriteLine(result.ToJson());
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.Message);
+                Console.WriteLine(e.Message);
+                Console.ReadLine();
+            }
+        }
+    }
 }
