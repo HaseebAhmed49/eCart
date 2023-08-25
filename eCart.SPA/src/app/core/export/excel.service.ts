6e9c87aed99d32ffdb2f2124af1f00d4ef07a079
HaseebAhmed7949 <66886913+HaseebAhmed49@users.noreply.github.com>
2023-08-25 23:53:14 +0500

    Added Code to export the data

--- eCart.SPA/src/app/core/export/excel.service.ts
+++ eCart.SPA/src/app/core/export/excel.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import * as XLSX from 'xlsx';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ExcelService {
 
+
   constructor() { }
+
+  exportToExcel(data: any[], filename: string, sheetName: string): void {
+    const worksheet: XLSX.WorkSheet = XLSX.utils.json_to_sheet(data);
+    const workbook: XLSX.WorkBook = { Sheets: { [sheetName]: worksheet }, SheetNames: [sheetName] };
+    const excelBuffer: any = XLSX.write(workbook, { bookType: 'xlsx', type: 'array' });
+    this.saveAsExcelFile(excelBuffer, filename);
+  }
+
+  private saveAsExcelFile(buffer: any, filename: string): void {
+    const blob = new Blob([buffer], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
+    const url = window.URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = filename;
+    a.click();
+    window.URL.revokeObjectURL(url);
+  }
 }
